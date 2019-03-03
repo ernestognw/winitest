40e99acb1f2b39c303450cd5fa520f61162f3979
ernestognw <ernestognw@gmail.com>
2019-03-02 18:22:47 -0600

    [Add] more seeders

--- server/graphql/resolvers/industry/queries.js
+++ server/graphql/resolvers/industry/queries.js
@@ -2,7 +2,7 @@ import { Industry } from "../../../db/models/";
 
 const industryQueries = {
   industryRandom: async () => {
-    const Qty = await Industry.find().count();
+    const Qty = await Industry.find().countDocuments();
     const random = Math.floor(Math.random() * Qty);
     return await Industry.findOne().skip(random);
   }
--- server/graphql/resolvers/profile/queries.js
+++ server/graphql/resolvers/profile/queries.js
@@ -2,7 +2,7 @@ import { Profile } from "../../../db/models/";
 
 const profileQueries = {
   profileRandom: async () => {
-    const Qty = await Profile.find().count();
+    const Qty = await Profile.find().countDocuments();
     const random = Math.floor(Math.random() * Qty);
     return await Profile.findOne().skip(random);
   }
